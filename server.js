5ea1e9982673cb2acda6a695b823475b861ea1e2
hjewnd2k <hjewnd2k>
2022-08-27 15:03:27 +0700

    video 5

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer(function(req, res) {
-    console.log("Request runner ...");
-    res.setHeader('Content-Type', 'text/html');
-    res.write("<h1>Hello ca nha</h1>")
-    res.write("<h2>Welcome to NodeJs</h2>")
-
-    res.end()
+app.get('/', (req, res) => {
+    res.send("<h1>Hello express</h1>")
 })
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server listening on port 3000');
+app.listen(port, (req, res) => {
+    console.log(`Server listening on ${port}`);
 })
